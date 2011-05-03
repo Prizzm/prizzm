12c0fdb3c6e5c1a4192646e081711d8a7aa9d597
Sidney Burks <sid137@gmail.com>
2011-05-03 22:20:00 +0200

    readded all ratings test items

--- lib/items.rb
+++ lib/items.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/env ruby
 require 'wrest'
+require 'hashie'
 require 'pp'
+Hash.send :include, Hashie::HashExtensions
 
 endpoint = "http://api.productwiki.com/connect/api.aspx?"
 PRODUCTWIKI_API_KEY = "fcafd9a662de4b099e4815e5e77739f1"
+data = []
+items = %w(iphone tablet wii toyota guitar)
+items.each do |item|
+  response = endpoint.to_uri.get(:op => "search", :key => PRODUCTWIKI_API_KEY, :q => item).deserialise.to_mash
+  results = response.pw_api_results.products.product
+  data << results
+end
 
-results = endpoint.to_uri.get(:op => "search", :key => PRODUCTWIKI_API_KEY, :q => 'iphone').deserialise
-
-
-pp results
+File.open('items.txt', 'w') do |f|
+  f.puts Marshal.dump data.flatten
+end
