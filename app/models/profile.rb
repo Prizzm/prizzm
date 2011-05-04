86716f592f72ca4eb467c7ea13d92af21579db5c
Sidney Burks <sid137@gmail.com>
2011-05-04 23:52:35 +0200

    added default profile photo if one is missing

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -16,4 +16,12 @@
 
 class Profile < ActiveRecord::Base
   belongs_to :user
+  before_save :ensure_photo_present
+
+
+  private
+
+  def ensure_photo_present
+    self.photo_url ||= "http://lorempixum.com/200/200/people/" + (1..10).to_a.rand.to_s 
+  end 
 end
