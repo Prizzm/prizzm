092669bc05e2bac91370e1904a3bd416de183c1a
Giang NGUYEN <ndgiang84@gmail.com>
2011-05-17 12:02:07 +0700

    Test get server name.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @server_name = request.env['SERVER_NAME']
     @user = current_user
     @interactions = current_user.interactions.order('created_at desc').all   
 
