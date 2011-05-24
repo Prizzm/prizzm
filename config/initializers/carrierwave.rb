58e99cc956d24a477dde010dabf93d701f869967
Sidney Burks <sid137@gmail.com>
2011-05-24 02:35:03 +0200

    carrierwave environmental config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,19 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => ENV['PRIZZM_S3_ACCESS_KEY_ID'],       # required
     :aws_secret_access_key  => ENV['PRIZZM_S3_SECRET_ACCESS_KEY'],       # required
+    :region                 => 'us-east-1'
   }
   config.fog_directory  = 'beta.prizzm.com'                     # required
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   config.fog_host = "http://#{config.fog_directory}.s3.amazonaws.com"
+  config.fog_public = true
+  
+  if Rails.env.production?
+     config.storage = :fog
+  elsif Rails.env.development?
+     config.storage = :file
+  elsif Rails.env.test?
+     config.storage = :file
+     config.enable_processing = false
+  end
 end
