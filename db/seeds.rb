fdb14833acd692d97951bc6046bfd5192d1c0f7c
Sidney Burks <sid137@gmail.com>
2011-06-15 15:39:11 +0200

    file store was breaking facebook logins on heroku, due to read-only ffilesystem;

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 # create a test user
-sidney = Factory :user, {:email => 'sid137@gmail.com', :password => 'password', :profile => Factory(:profile, {:first_name => 'Sidney', :last_name => 'Burks'}) } 
+#sidney = Factory :user, {:email => 'sid137@gmail.com', :password => 'password', :profile => Factory(:profile, {:first_name => 'Sidney', :last_name => 'Burks'}) } 
 test = Factory :user, {:email => 'test@test.com', :password => 'password', :profile => Factory(:profile, {:first_name => 'Test', :last_name => 'User'}) } 
-sidney.make_real
+#sidney.make_real
 test.make_real
 
 bryna_nicole = Company.create({
