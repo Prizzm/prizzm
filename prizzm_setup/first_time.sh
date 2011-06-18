a928d08cbc818a22f051176476da503e3d5fab92
Sidney Burks <sid137@gmail.com>
2011-06-18 03:01:25 +0200

    refactor setup

---
+++ prizzm_setup/first_time.sh
@@ -0,0 +1,23 @@
+
+
+#  First time setup
+#
+
+# Install project ruby version if not installed, and activated it
+#rvm_install_on_use_flag=1
+
+# Compile ruby for 64-bit architecture on MacOSX
+#rvm_archflags="-arch x86_64"
+
+# Compile faster
+#rvm_make_flags="-j 3"
+
+# return to defaul RVM when leaving project
+#rvm_project_rvmrc_default=1
+
+#rvm use 1.9.2-p180 #@$PROJECT_NAME --create
+
+# Need to run only on first install
+#echo "${alert} Installing project gems..."
+#bundle install --binstubs  | grep -v 'Using' | grep -v 'complete' | sed '/^$/d'
+
