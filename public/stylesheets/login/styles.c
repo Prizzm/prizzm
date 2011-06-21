2061314cba5fff4341dc1dc350ff64b9fab335c0
Sidney Burks <sid137@gmail.com>
2011-06-21 16:42:20 +0200

    forgot to add fonts

---
+++ public/stylesheets/login/styles.c
@@ -0,0 +1,28 @@
+/* ============================== */
+/* ! Layout for desktop version   */
+/* ============================== */
+	
+	
+	
+/* ============================= */
+/* ! Layout for mobile version   */
+/* ============================= */
+
+@media handheld, only screen and (max-width: 767px) {
+
+	body {
+		
+	}
+
+}
+
+
+/* ========================================== */
+/* ! Provide higher res assets for iPhone 4   */
+/* ========================================== */
+
+@media only screen and (-webkit-min-device-pixel-ratio: 2) { 
+
+
+
+}
