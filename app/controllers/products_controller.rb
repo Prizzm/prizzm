607009bef6b918ca951e14a4e3c99dc9d78b6b6c
Sidney Burks <sid137@gmail.com>
2011-07-06 22:45:26 +0200

    autocomplete works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,19 +14,13 @@ class ProductsController < ApplicationController
     # limit results to 6 or so, we we don't reach browser parallel download
     products = Product.order('name ASC')
     #products = Product.all(:conditions => ["name like ?", params[:term] +'%'])
-    #products = Product.all(:conditions => ["name like ?", params[:query] +'%'])
+    products = Product.all(:conditions => ["name like ?", params[:query] +'%'])
     render :json  => autocomplete_info_for(products)   
   end 
 
 private
   
   def autocomplete_info_for products
-    #query = params[:query]
-    #suggestions =  products.collect { |p| p.name}
-    #data = products.collect do |p| 
-      #{:label => p.label, :main_image_thumb => p.main_image_thumb, :customer_count => p.customer_count, :id => p.id, :rating => p.rating}
-    #end
-    #output = {:query => query, :suggestions => suggestions, :data => data }
     results = products.to_json(:methods => [:label, :main_image_thumb, :customer_count], :only => [:id, :rating])
   end
 end
