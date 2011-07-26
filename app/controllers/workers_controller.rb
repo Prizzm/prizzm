1c5bb5c10a26f6337e27305aaae3a4bf894b9056
Sidney Burks <sid137@gmail.com>
2011-07-26 07:47:51 +0200

    retest

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,9 +1,10 @@
 class WorkersController < ApplicationController
 
   def test
-    worker = HelloWorker.new
-    worker.some_param = "Passing in arrg"
-    worker.queue
+    #worker = HelloWorker.new
+    #worker.some_param = "Passing in arrg"
+    #worker.queue
+    HelloJob.perform
   end
 
 end
