35c2129fc4a713281f69f0ca3957f7301d01cd4b
Sidney Burks <sid137@gmail.com>
2011-07-26 10:23:51 +0200

    test autoscalling

--- app/workers/hello_worker.rb
+++ app/workers/hello_worker.rb
@@ -1,16 +1,12 @@
 require 'pp'
 class HelloWorker 
-  #extend HerokuResqueAutoScale if Rails.env.production?
+  extend HerokuResqueAutoScale if Rails.env.production?
 
   @queue = :test_queue
 
   def self.perform
-    puts "Working"
-    puts "access key: #{ENV['AMAZON_ACCESS_KEY_ID']}"
-    puts " secret key #{ENV['AMAZON_SECRET_ACCESS_KEY']}"
-    pp ENV
     @events = Notification.all.to_a
     ses = AWS::SimpleEmailService.new
-    ses.send_email(:to => "sid137@gmail.com", :from => "notifications@prizzm.com", :subject => "Sent from Resque", :body_text => "#{pp @events}" )
+    ses.send_email(:to => "sid137@gmail.com", :from => "notifications@prizzm.com", :subject => "Sent from Resque", :body_text => "#{pp @events}    \n\n\n\n\n#{ENV}" )
   end
 end
