c6fadcdda4166f3cd6e37fe261fba574d367cef4
Sidney Burks <sid137@gmail.com>
2011-08-02 20:06:44 +0200

    comments implemented with notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ActiveRecord::Base
-
   include ActsAsCommentable::Comment
 
+  after_create :log_comment
+
+  belongs_to :user
   belongs_to :commentable, :polymorphic => true
 
   default_scope :order => 'created_at ASC'
@@ -10,6 +12,19 @@ class Comment < ActiveRecord::Base
   # want user to vote on the quality of comments.
   #acts_as_voteable
 
-  # NOTE: Comments belong to a user
-  belongs_to :user
+protected
+
+  #  This method dectects which attributes were changed from the model update,
+  #  and sends a notification signalling each attribute change.  This provides
+  #  several benefits over signalling the atribute changes in the controller.
+  #  First, we can emit notifications directly from teh model, thus aid in test
+  #  data generation and unit testing.  Second, we can have the notifications all
+  #  in one central place, as opposed to being scattered throughout the
+  #  controllers
+
+
+  def log_comment
+    event_name = "user_comment_#{self.commentable_type.underscore}"
+    ActivityLogger.send(event_name,  :from => self.user, :for => [self.commentable], :data => self)
+  end
 end
