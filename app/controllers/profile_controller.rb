0f927043d901e3802820b9d76c94669cdb1935b3
Sidney Burks <sid137@gmail.com>
2011-08-05 14:23:45 +0200

    geoff layouts

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,5 @@ class ProfileController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render 'my_profile' and return if @user == current_user
-    render 'public_profile'
   end 
 end
