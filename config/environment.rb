4744f9bcbe72f76b2fea9411c844e71ebb646259
Sidney Burks <sid137@gmail.com>
2011-08-08 22:01:16 +0200

    pretty printing

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Prizzm::Application.initialize!
+
+
+if ENV['HEROKU_APP'] ==  'prizzm-development'
+  Haml::Template.options[:ugly] = false
+end
