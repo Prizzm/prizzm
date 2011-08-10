d4e6afd47a6446d5d4b6a667069d00c8401f6615
Sidney Burks <sid137@gmail.com>
2011-08-10 22:14:05 +0200

    introducing some invalid html here.. block elements insie inline elements

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -4,5 +4,12 @@ module ProfileHelper
     @user == current_user
   end
 
+  def review_instruction_for item
+    if item.review.blank?
+      "Write review"
+    else
+      "Read review"
+    end
+  end
 
 end
