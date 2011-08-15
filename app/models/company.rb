2e3a6f098bece3fa41828ec159fc87a5ce4fa30f
Giang NGUYEN <ndgiang84@xiga.info>
2011-08-15 11:23:44 +0700

    Improved company address.

--- app/models/company.rb
+++ app/models/company.rb
@@ -9,4 +9,30 @@ class Company < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true, :approximate_ascii => true, :reserved_words => %(show delete), :allow_nil => true 
   
   acts_as_followable
+
+  ADD_TYPES = %w(street_address phone_number website twitter facebook)
+
+  ADD_ICON = {
+    "street_address" => "street",
+    "phone_number"   => "phone",
+    "website"        => "world_link",
+    "twitter"        => "tt",
+    "facebook"       => "fb"
+  }
+
+  ADD_CLASS = {
+    "street_address" => "button",
+    "phone_number"   => "call",
+    "website"        => "button",
+    "twitter"        => "button",
+    "facebook"       => "button"
+  }
+
+  ADD_TEXT = {
+    "street_address" => "Go to",
+    "phone_number"   => "Call",
+    "website"        => "Go to",
+    "twitter"        => "Go to",
+    "facebook"       => "Go to"
+  }
 end
