3e3f6639b4bfc6eb05a04694c448de0573733e7e
Giang NGUYEN <ndgiang84@xiga.info>
2011-08-27 00:53:14 +0700

    Added sending, accepting invitation.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,13 @@
 class InvitationsController < Devise::InvitationsController
+  def create
+    emails = params[:user][:email].reject { |email| email.blank? }.uniq
+    emails.each { |email| resource_class.invite!({ :email => email }, current_inviter) }
+
+    #if resource.errors.empty?
+    #  set_flash_message :notice, :send_instructions, :email => self.resource.email
+    #  respond_with resource, :location => after_invite_path_for(resource)
+    #else
+    #  respond_with_navigational(resource) { render_with_scope :new }
+    #end
+  end
 end
