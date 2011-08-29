b81c860a5b1a7e6f09d276babd45c023cad442fa
Sidney Burks <sid137@gmail.com>
2011-08-29 11:30:31 -0700

    added privacy controls to cases

--- app/helpers/privacy_helper.rb
+++ app/helpers/privacy_helper.rb
@@ -1,5 +1,5 @@
 module PrivacyHelper
   def privacy_control_for object
-    render :partial => 'privacy/privacy_controls', :locals => {:object => object }
+    render :partial => 'privacy/privacy_controls', :locals => {:object => object } if (object.user == current_user)
   end
 end
