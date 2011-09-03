84704443afd7d1559a345137c9ccabaa2e6c9017
Charles Oppenheimer <choppen5@yahoo.com>
2011-09-02 23:01:05 -0700

    removed popular tags that wasn't functional

---
+++ db/schema.rb
@@ -0,0 +1,262 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110831044703) do
+
+  create_table "addresses", :force => true do |t|
+    t.text     "address"
+    t.string   "address_type"
+    t.string   "description"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cases", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.date     "date"
+    t.string   "case_reference"
+    t.string   "privacy"
+    t.integer  "user_id"
+    t.integer  "company_id"
+    t.integer  "item_id"
+    t.string   "cached_slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "workflow_state"
+  end
+
+  add_index "cases", ["cached_slug"], :name => "index_cases_on_cached_slug", :unique => true
+
+  create_table "comments", :force => true do |t|
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.string   "title",            :limit => 50, :default => ""
+    t.text     "body",                           :default => ""
+    t.string   "subject",                        :default => ""
+    t.integer  "user_id"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "hidden"
+  end
+
+  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
+  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "address"
+    t.string   "phone_number"
+    t.string   "secondary_number"
+    t.text     "url"
+    t.string   "industry"
+    t.string   "contact_name"
+    t.string   "cached_slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "companies", ["cached_slug"], :name => "index_companies_on_cached_slug", :unique => true
+
+  create_table "company_images", :force => true do |t|
+    t.integer  "company_id"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "follows", :force => true do |t|
+    t.integer  "followable_id",                      :null => false
+    t.string   "followable_type",                    :null => false
+    t.integer  "follower_id",                        :null => false
+    t.string   "follower_type",                      :null => false
+    t.boolean  "blocked",         :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "follows", ["followable_id", "followable_type"], :name => "fk_followables"
+  add_index "follows", ["follower_id", "follower_type"], :name => "fk_follows"
+
+  create_table "images", :force => true do |t|
+    t.integer  "item_id"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "item_images", :force => true do |t|
+    t.integer  "item_id"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", :force => true do |t|
+    t.text     "url"
+    t.text     "review"
+    t.text     "description"
+    t.string   "sku"
+    t.string   "name"
+    t.string   "privacy"
+    t.string   "invitation_status"
+    t.string   "possession_status"
+    t.integer  "rating"
+    t.integer  "position"
+    t.integer  "asking_price"
+    t.string   "cached_slug"
+    t.integer  "user_id"
+    t.integer  "company_id"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "items", ["cached_slug"], :name => "index_items_on_cached_slug", :unique => true
+
+  create_table "product_images", :force => true do |t|
+    t.integer  "product_id"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "product_invitations", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "product_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.date     "purchase_date"
+    t.string   "amount"
+    t.text     "content"
+    t.string   "encrypted_id"
+    t.string   "sent"
+    t.datetime "sent_on"
+    t.string   "accepted"
+  end
+
+  create_table "product_reviews", :force => true do |t|
+    t.integer  "product_invitation_id"
+    t.integer  "product_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", :force => true do |t|
+    t.integer  "company_id"
+    t.string   "name"
+    t.string   "product_type"
+    t.string   "price"
+    t.string   "upc"
+    t.text     "url"
+    t.text     "description"
+    t.integer  "rating"
+    t.string   "cached_slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["cached_slug"], :name => "index_products_on_cached_slug", :unique => true
+
+  create_table "settings", :force => true do |t|
+    t.string   "var",                      :null => false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", :limit => 30
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
+
+  create_table "subscriptions", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "subscribable_id"
+    t.string   "subscribable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => ""
+    t.string   "reset_password_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "access_token"
+    t.string   "tt_token"
+    t.string   "tt_secret"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.string   "photo"
+    t.string   "location"
+    t.string   "cached_slug"
+    t.string   "invitation_token",     :limit => 60
+    t.datetime "invitation_sent_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "company_id"
+  end
+
+  add_index "users", ["cached_slug"], :name => "index_users_on_cached_slug", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["invitation_token"], :name => "index_users_on_invitation_token"
+  add_index "users", ["invited_by_id"], :name => "index_users_on_invited_by_id"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+end
