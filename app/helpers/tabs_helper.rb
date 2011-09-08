90c653883fcd78d9cc7d2e1c97310c5ea0d4d69c
Geoffrey Chong <ohsogeoff@gmail.com>
2011-09-08 00:49:35 -0700

    Updated login page

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,7 +1,8 @@
-module TabsHelper
-
-  def tab_for(path)
-    "active" if current_page?(path)
+module TabsHelper  
+  # Return "active" if path points to the active (current) page, nothing otherwise.
+  def active_if_current(path)
+    current_page?(path) ? "active" : ''
   end
+  
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+  
   // Privacy ccontrols ffor items.  Control is a ajax link wich sends a put
   // request to toggle te privacy.  This script sets a return handler, to show
   // the updateed privacy mode, and toggle the css class.
