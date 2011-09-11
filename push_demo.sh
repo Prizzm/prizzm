0fcd30c542fff1bfd151c17b9257b1afdd4023ac
Sidney Burks <sid137@gmail.com>
2011-09-11 14:22:59 -0700

    modifying deployment scripts

--- push_demo.sh
+++ push_demo.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+. ./prizzm_setup/functions
 
+server=${1:-development}
+asset_host=assets-$server.prizzm.com
+server_mongolab_uri=`$server config | grep MONGOLAB_URI`
+echo $server_mongolab_uri
 export PUSH_DEMO=true
-rds
+export S3_ASSET_HOST=$asset_host
+echo $server
+echo $S3_ASSET_HOST
+#rds $server
 unset PUSH_DEMO
-db push
-rds
+unset S3_ASSET_HOST
+#db push $server
+#rds
