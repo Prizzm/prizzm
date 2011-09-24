74c0fb7153032add1d5f4e7a36588163fa307fb3
Tiago <tpaiva86@gmail.com>
2011-09-24 10:12:36 -0700

    pusher working

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Prizzm::Application.initialize!
 ENV['MONGOHQ_URL'] = 'mongodb://prizzm:prizzm@staff.mongohq.com:10046/prizzm'
+ENV['MONGOLAB_URI'] = 'mongodb://heroku_app936124:clnt6mq151j6u9k1a23v4mk97f@dbh44.mongolab.com:27447/heroku_app936124'
 
 if ENV['HEROKU_APP'] ==  'prizzm-development'
   Haml::Template.options[:ugly] = false
