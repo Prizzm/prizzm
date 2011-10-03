ed44c755aecc8f514c16c9bd407333a418ac2d0c
Geoffrey Chong <ohsogeoff@gmail.com>
2011-10-03 16:54:45 -0700

    people following list

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -25,6 +25,20 @@ class PeopleController < ApplicationController
                  .limit(10)
 
   end
+  
+  def following_list
+    @items = Item.joins('INNER JOIN follows ON follows.follower_id = items.user_id')
+                 .where(
+                   :follows => {
+                     :followable_type => 'User',
+                     :follower_type   => 'User',
+                     :followable_id   => current_user.id
+                   },
+                   :privacy => "public"
+                 )
+                 .limit(10)
+
+  end
 
 
   def search
