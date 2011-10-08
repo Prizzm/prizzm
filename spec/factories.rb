f0240d23141e7a7519cf789585879d51466d36fc
Iszak Bryan <iszak.bryan@gmail.com>
2011-10-08 11:59:03 +0100

    Fix for breaking of database seeding

--- spec/factories.rb
+++ spec/factories.rb
@@ -50,6 +50,7 @@ Factory.define :user do |user|
   user.password_confirmation {|u| u.password }
   user.first_name { Forgery::Name.first_name }
   user.last_name { Forgery::Name.last_name }
+  user.username { |u| "#{u.first_name}_#{u.last_name}" }
   user.email {|u| "#{u.first_name}_#{u.last_name}@prizzm.com"}
   user.phone_number { Forgery::Address.phone }
   user.location { Forgery::Address.street_address }
